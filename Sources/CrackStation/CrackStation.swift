ac94b9d88b2f19beacf26b5cf280e544fe1f7270
Hash7ag <Hash7ag@protonmail.com>
2022-10-24 15:47:35 -0700

    Implement sha1 & sha256 cracking based on hash dataset

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -1,6 +1,27 @@
-public struct CrackStation {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public class CrackStation: Decrypter {
+    private var dataDict: Dictionary<String, String> = [:]
+
+    required public init() {
+        if let path = Bundle.module.url(forResource: "data", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: path)
+                let jsonData = try JSONSerialization.jsonObject(with: data)
+                if let jsonResult = jsonData as? Dictionary<String, String> {
+                    self.dataDict = jsonResult
+                }
+                else {
+                    print ("\"data.json is empty.\"")
+                }
+            }
+            catch {
+                print ("\(error)\nSerialization JSON failed. \"data.json\" might be corruption.")
+            }
+        }
+    }
+
+    public func decrypt(shaHash: String) -> String? {
+        return self.dataDict[shaHash]
     }
 }
