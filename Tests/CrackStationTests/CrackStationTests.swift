64a4befd10a97422baf3f170f3cc00214aabc753
Hash7ag <Hash7ag@protonmail.com>
2022-10-24 15:49:05 -0700

    Add unit tests

--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -1,11 +1,56 @@
 import XCTest
-@testable import CrackStation
+import CrackStation
+
+import Crypto
 
 final class CrackStationTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CrackStation().text, "Hello, World!")
+    private let crackStation: Decrypter = CrackStation()
+
+    // MARK: - Happy path
+    func testAllOneLetterSha1Permutations() throws {
+        for letter in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" {
+            // Given
+            let password = String(letter)
+            let shaHash = encrypt(password)
+
+            // When
+            let crackedPassword = crackStation.decrypt(shaHash: shaHash)
+
+            // Then
+            XCTAssertEqual(crackedPassword, password)
+        }
+    }
+
+    // MARK: - Edge cases / rainy day scenarios
+    func testEmptyString() throws {
+        // Given
+        let password = ""
+        let shaHash = encrypt(password)
+
+        // When
+        let crackedPassword = crackStation.decrypt(shaHash: shaHash)
+
+        // Then
+        XCTAssertEqual(crackedPassword, nil)
+    }
+
+    func testInvalidShaHash() throws {
+        // Given
+        let password = "Hello-World"
+        let shaHash = String(encrypt(password).suffix(5))
+
+        // When
+        let crackedPassword = crackStation.decrypt(shaHash: shaHash)
+
+        // Then
+        XCTAssertEqual(crackedPassword, nil)
+    }
+
+    // MARK: - Helpers
+    private func encrypt(_ password: String) -> String {
+        let dataToHash = Data(password.utf8)
+        let shaHashDescription = String(Insecure.SHA1.hash(data: dataToHash).description)
+        let shaHash = String(shaHashDescription.suffix(40))
+        return shaHash
     }
 }
